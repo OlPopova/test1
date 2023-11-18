99afc3ea997791165e0430c68ab679e1534b1b52
Olga Popova <mitsu89@mail.ru>
2023-11-18 16:37:55 +0300

    begin

---
+++ src/Knight.java
@@ -0,0 +1,25 @@
+
+class Knight {
+    private String name = "Sir Thanks-A-Lot";
+    private String weapon = "Long Sword";
+    private Boolean isGoingToSavePrincess = true;
+
+    public static void main(String[] args) {
+}
+    public void goAndSaveThePrincess() {
+        sharpenBlade();
+        getFood();
+        assembleTeam();
+        System.out.println("Da idu uzhe...");
+    }
+
+    private void sharpenBlade() {
+        System.out.println("Tochim mech");
+    }
+    private void getFood() {
+        System.out.println("Sobiraem conservy");
+    }
+    private void assembleTeam() {
+        System.out.println("Budim oruzhenostsa");
+    }
+}
